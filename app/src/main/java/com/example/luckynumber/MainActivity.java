644567dd65ccb4b788336cc7d76c286d66ecbf32
Gabriela Cleim <gabriela.cleims@gmail.com>
2023-09-01 19:14:22 -0300

    app no esquema

--- app/src/main/java/com/example/luckynumber/MainActivity.java
+++ app/src/main/java/com/example/luckynumber/MainActivity.java
@@ -2,23 +2,27 @@ package com.example.luckynumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout primaryLinear, secondaryLinear,linear1, linear2;
     EditText edt_one, edt_two, edt_three;
 
-    TextView edtnumSor;
-    Button btn;
+    TextView edtnumSor, textFinal;
+    Button btn, btnBack;
 
 
     int edtOne;
@@ -29,37 +33,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
         findIds();
-
-
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //dtOne =  edt_one.getText().toString();
-                //edtTwo = edt_two.getText().toString();
-                edtOne = Integer.parseInt(String.valueOf(edt_one.getText()));
-                edtTwo = Integer.parseInt(String.valueOf(edt_two.getText()));
-                edtThree = Integer.parseInt(String.valueOf(edt_three.getText()));
+                edtOne = Integer.parseInt(edt_one.getText().toString());
+                edtTwo = Integer.parseInt(edt_two.getText().toString());
+                edtThree = Integer.parseInt(edt_three.getText().toString());
 
                 if(edtOne > 0){
 
+                    List<String> array = new ArrayList<>();
+                    int i;
 
-                    for (int i = 0; i < edtOne; i++) {
+                    for (i = 0; i < edtOne; i++) {
                         int num = (int)(Math.random() * edtThree) + edtTwo;
-                        primaryLinear.setVisibility(View.GONE);
-                        secondaryLinear.setVisibility(View.VISIBLE);
-                        edtnumSor.setText(String.valueOf(num));
+                        array.add(String.valueOf(num));
                         System.out.println(num);
-
-
                     }
 
+                    if(String.valueOf(i).length() == String.valueOf(edtOne).length()){
+                        primaryLinear.setVisibility(View.GONE);
+                        secondaryLinear.setVisibility(View.VISIBLE);
+                        edtnumSor.setText(array.toString());
 
+                        if(imm.isActive()){
+                            imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+                        }
 
+                    }
+                    if(edtOne >= 2){
+                        primaryLinear.setVisibility(View.GONE);
+                        secondaryLinear.setVisibility(View.VISIBLE);
+                        textFinal.setText("Os números sorteados são: ");
+                        edtnumSor.setText(array.toString());
+                    }
                 }
+            }
+        });
 
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                primaryLinear.setVisibility(View.VISIBLE);
+                secondaryLinear.setVisibility(View.GONE);
+                edt_one.setText("");
+                edt_two.setText("");
+                edt_three.setText("");
             }
         });
 
@@ -70,8 +93,11 @@ public class MainActivity extends AppCompatActivity {
         edt_two = findViewById(R.id.edt_two);
         edt_three = findViewById(R.id.edt_three);
         edtnumSor = findViewById(R.id.edtnumSor);
+        textFinal = findViewById(R.id.textFinal);
         btn = findViewById(R.id.btn);
+        btnBack = findViewById(R.id.btnBack);
         primaryLinear = findViewById(R.id.primaryLinear);
         secondaryLinear = findViewById(R.id.secondaryLinear);
+
     }
 }
